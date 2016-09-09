8e5ca434a50abd42be88d177b5ef0c0e7bf94fe9
andrew morton <drewish@katherinehouse.com>
2016-09-09 16:36:30 -0600

    Don't call underscore on param names
    It's much more understandable to have `let(:fooBar)` and `parameter :fooBar`
    use the same names.

--- lib/swagger_rails/test_visitor.rb
+++ lib/swagger_rails/test_visitor.rb
@@ -26,7 +26,7 @@ module SwaggerRails
 
         parameter
           .slice(:name, :in)
-          .merge(value: test.send(parameter[:name].to_s.underscore))
+          .merge(value: test.send(parameter[:name].to_s))
       end
     end
 
