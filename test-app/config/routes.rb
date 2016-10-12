941b195f689cffcb047c504a010d113d4eb2bf25
richie <richie@navigatingcancer.com>
2016-10-11 23:00:12 -0700

    In .travis.yml, use Rails 3 version that's compatable with Ruby 2.2

--- test-app/config/routes.rb
+++ test-app/config/routes.rb
@@ -1,8 +1,4 @@
 TestApp::Application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-
-  mount Rswag::Api::Engine => '/api-docs'
-
   resources :blogs, defaults: { :format => :json }
 
   mount Rswag::Api::Engine => 'api-docs'
