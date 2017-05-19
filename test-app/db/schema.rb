353be669e4ff118b082a7b9227d95a399b87c41e
domaindrivendev <domaindrivendev@gmail.com>
2017-05-19 11:11:13 -0700

    Removed some deprecation warnings

--- test-app/db/schema.rb
+++ test-app/db/schema.rb
@@ -13,10 +13,10 @@
 ActiveRecord::Schema.define(version: 20160218212104) do
 
   create_table "blogs", force: :cascade do |t|
-    t.string "title"
+    t.string "title", limit: 255
     t.text "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
