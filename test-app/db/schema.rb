4c708295b947ed62138b570db961b68916516562
domaindrivendev <domaindrivendev@gmail.com>
2018-04-23 21:53:19 -0700

    Update travis config to test against rails 5.2.0

--- test-app/db/schema.rb
+++ test-app/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -9,16 +8,16 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160218212104) do
+ActiveRecord::Schema.define(version: 2016_02_18_212104) do
 
-  create_table "blogs", :force => true do |t|
-    t.string   "title"
-    t.text     "content"
-    t.string   "thumbnail"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "blogs", force: :cascade do |t|
+    t.string "title", limit: 255
+    t.text "content"
+    t.string "thumbnail", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
