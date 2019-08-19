be6503cc81337e3e319ff4f6a2c8159c1960a238
Christian Heimke <christian.heimke@loumaris.com>
2019-08-19 21:11:55 +0200

    updating travis config

--- ci/test.sh
+++ ci/test.sh
@@ -10,15 +10,15 @@ echo ''
 
 echo '##### rswag-api #####'
 cd $ROOT_PATH/rswag-api
-bundle exec rspec
+xvfb-run bundle exec rspec
 
 echo '##### rswag-specs #####'
 cd $ROOT_PATH/rswag-specs
-bundle exec rspec
+xvfb-run bundle exec rspec
 
 echo '##### rswag-ui #####'
 cd $ROOT_PATH/rswag-ui
-bundle exec rspec
+xvfb-run bundle exec rspec
 
 echo '####################'
 echo 'Integration Tests'
@@ -27,8 +27,8 @@ echo ''
 
 echo '##### test-app #####'
 cd $ROOT_PATH/test-app
-bundle exec rake db:migrate db:test:prepare
-bundle exec rspec
+xvfb-run bundle exec rake db:migrate db:test:prepare
+xvfb-run bundle exec rspec
 
 # Cleanup
 cd $ROOT_PATH
