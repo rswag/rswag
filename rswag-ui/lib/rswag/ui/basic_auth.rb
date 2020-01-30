e21f786926c5261893922001bf2c7c78369675a4
Chris Miller <chrismillersd@gmail.com>
2020-01-29 16:28:06 -0800

    fix(references): update reference to Rswag::Ui::BasicAuth and env_matching_path environment accessor

--- rswag-ui/lib/rswag/ui/basic_auth.rb
+++ rswag-ui/lib/rswag/ui/basic_auth.rb
@@ -9,21 +9,18 @@ module Rswag
     #
     class BasicAuth < ::Rack::Auth::Basic
       def call(env)
-        return @app.call(env) unless env_matching_path
+        return @app.call(env) unless env_matching_path(env)
 
         super(env)
       end
 
       private
 
-      def env_matching_path
-        Rswag::Ui.config.swagger_endpoints[:urls].find do |endpoint|
-          base_path(endpoint[:url]) == env_base_path
+      def env_matching_path(env)
+        path = base_path(env['PATH_INFO'])
+        Rswag::Ui.config.config_object[:urls].find do |endpoint|
+          base_path(endpoint[:url]) == path
         end
-      end
-
-      def env_base_path
-        base_path(env['PATH_INFO'])
       end
 
       def base_path(url)
