c3422efeb93aed883a092df76443ea471afb2a3a
Robert Schulze <robert@dotless.de>
2022-01-18 15:15:46 +0100

    keep existing metadata in requestBody.content.mime

--- rswag-specs/lib/rswag/specs/swagger_formatter.rb
+++ rswag-specs/lib/rswag/specs/swagger_formatter.rb
@@ -61,7 +61,7 @@ module Rswag
                     value[:requestBody] = { content: {} } unless value.dig(:requestBody, :content)
                     value[:requestBody][:required] = true if schema_param[:required]
                     mime_list.each do |mime|
-                      value[:requestBody][:content][mime] = { schema: schema_param[:schema] }
+                      (value[:requestBody][:content][mime] ||= {})[:schema] = schema_param[:schema]
                     end
                   end
 
