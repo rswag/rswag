6e59fc98156e1986a8385b521939fa5579ad2e58
ydah <13041216+ydah@users.noreply.github.com>
2022-08-17 12:42:52 +0900

    s/legay/legacy/

--- test-app/spec/integration/blogs_spec.rb
+++ test-app/spec/integration/blogs_spec.rb
@@ -99,7 +99,7 @@ RSpec.describe 'Blogs API', type: :request, swagger_doc: 'v1/swagger.json' do
         #Legacy
         examples 'application/json' => {
           id: 1,
-          title: 'Hello legay world!',
+          title: 'Hello legacy world!',
           content: 'Hello legacy world and hello universe. Thank you all very much!!!',
           thumbnail: 'legacy-thumbnail.png'
         }
