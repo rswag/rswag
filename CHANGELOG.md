3ffb7ba2f6d2c6ff59964747d470f46549afdf65
Roman Blanco <rblanco@redhat.com>
2023-10-29 17:33:41 +0000

    feat: warning about methods renaming

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ### Added
 
+- Add warning about methods renaming (https://github.com/rswag/rswag/pull/688)
+
 ### Changed
 
 ### Documentation
--- rswag-specs/lib/rswag/specs/configuration.rb
+++ rswag-specs/lib/rswag/specs/configuration.rb
@@ -8,6 +8,7 @@ module Rswag
       end
 
       def swagger_root
+        ActiveSupport::Deprecation.warn('Rswag::Specs: WARNING: The method will be renamed to "openapi_root" in v3.0')
         @swagger_root ||= begin
           if @rspec_config.swagger_root.nil?
             raise ConfigurationError, 'No swagger_root provided. See swagger_helper.rb'
@@ -18,6 +19,7 @@ module Rswag
       end
 
       def swagger_docs
+        ActiveSupport::Deprecation.warn('Rswag::Specs: WARNING: The method will be renamed to "openapi_specs" in v3.0')
         @swagger_docs ||= begin
           if @rspec_config.swagger_docs.nil? || @rspec_config.swagger_docs.empty?
             raise ConfigurationError, 'No swagger_docs defined. See swagger_helper.rb'
@@ -28,6 +30,7 @@ module Rswag
       end
 
       def swagger_dry_run
+        ActiveSupport::Deprecation.warn('Rswag::Specs: WARNING: The method will be renamed to "rswag_dry_run" in v3.0')
         return @swagger_dry_run if defined? @swagger_dry_run
         if ENV.key?('SWAGGER_DRY_RUN')
           @rspec_config.swagger_dry_run = ENV['SWAGGER_DRY_RUN'] == '1'
@@ -36,6 +39,7 @@ module Rswag
       end
 
       def swagger_format
+        ActiveSupport::Deprecation.warn('Rswag::Specs: WARNING: The method will be renamed to "openapi_format" in v3.0')
         @swagger_format ||= begin
           @rspec_config.swagger_format = :json if @rspec_config.swagger_format.nil? || @rspec_config.swagger_format.empty?
           raise ConfigurationError, "Unknown swagger_format '#{@rspec_config.swagger_format}'" unless [:json, :yaml].include?(@rspec_config.swagger_format)
@@ -45,6 +49,7 @@ module Rswag
       end
 
       def get_swagger_doc(name)
+        ActiveSupport::Deprecation.warn('Rswag::Specs: WARNING: The method will be renamed to "get_openapi_spec" in v3.0')
         return swagger_docs.values.first if name.nil?
         raise ConfigurationError, "Unknown swagger_doc '#{name}'" unless swagger_docs[name]
 
@@ -57,6 +62,7 @@ module Rswag
       end
 
       def swagger_strict_schema_validation
+        ActiveSupport::Deprecation.warn('Rswag::Specs: WARNING: The method will be renamed to "openapi_strict_schema_validation" in v3.0')
         @swagger_strict_schema_validation ||= (@rspec_config.swagger_strict_schema_validation || false)
       end
     end
