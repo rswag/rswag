43b82aac6d4997ef18da3510054929572cfe8fb7
Bohdan Zhuravel <bohdan@zhuravel.bz>
2024-08-13 12:41:09 +0300

    Fix deprecation warning in Rails 7.1+

--- test-app/app/models/blog.rb
+++ test-app/app/models/blog.rb
@@ -6,7 +6,13 @@ class Blog < ActiveRecord::Base
   alias_attribute :headline, :title
   alias_attribute :text, :content
 
-  enum status: [:draft, :published, :archived]
+  # Rails 7.1 deprecated defining enums with keyword arguments.
+  # See https://github.com/rails/rails/pull/50987
+  if Gem::Version.new(Rails.version) >= Gem::Version.new("7.1")
+    enum :status, [:draft, :published, :archived]
+  else
+    enum status: [:draft, :published, :archived]
+  end
 
   def as_json(_options)
     {
