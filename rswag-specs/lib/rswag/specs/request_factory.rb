c7293883c7b0437b0a5841fe56c1aec3ab7baede
Joel Tannas <jtannas@gmail.com>
2025-02-21 15:03:51 -0700

    distinguish between parameter definitions and values

--- rswag-specs/lib/rswag/specs/request_factory.rb
+++ rswag-specs/lib/rswag/specs/request_factory.rb
@@ -22,61 +22,61 @@ module Rswag
         'Host' => 'HTTP_HOST'
       }.freeze
       EMPTY_PARAMETER_GROUPS = { body: [], path: [], query: [], formData: [], header: [] }.freeze
-      attr_accessor :example, :metadata, :params, :headers
+      attr_accessor :example, :metadata, :param_values, :headers
 
       def initialize(metadata, example, config = ::Rswag::Specs.config)
         @config = config
         @example = example
         @metadata = metadata
-        @params = example.respond_to?(:request_params) ? example.request_params : {}
+        @param_values = example.respond_to?(:request_params) ? example.request_params : {}
         @headers = example.respond_to?(:request_headers) ? example.request_headers : {}
       end
 
       def build_request
         openapi_spec = @config.get_openapi_spec(metadata[:openapi_spec])
-        parameters = expand_parameters(metadata, openapi_spec, example)
-        parameter_groups = parameters.group_by { |p| p[:in] }.reverse_merge(EMPTY_PARAMETER_GROUPS)
+        parameter_definitions = expand_definitions(metadata, openapi_spec, example)
+        definition_groups = parameter_definitions.group_by { |d| d[:in] }.reverse_merge(EMPTY_PARAMETER_GROUPS)
         request = {
           verb: metadata[:operation][:verb],
-          path: build_path_string(metadata, openapi_spec, parameter_groups[:path]) +
-                build_query_string(parameter_groups[:query]),
-          headers: build_headers(metadata, openapi_spec, parameter_groups[:header], example)
+          path: build_path_string(metadata, openapi_spec, definition_groups[:path]) +
+                build_query_string(definition_groups[:query]),
+          headers: build_headers(metadata, openapi_spec, definition_groups[:header], example)
         }
         request[:payload] = case request[:headers]['CONTENT_TYPE']
                             when nil then nil
                             when 'application/x-www-form-urlencoded', 'multipart/form-data'
-                              build_form_payload(parameter_groups[:formData])
+                              build_form_payload(definition_groups[:formData])
                             when %r{\Aapplication/([0-9A-Za-z._-]+\+json\z|json\z)}
-                              build_raw_payload(parameter_groups[:body])&.to_json
+                              build_raw_payload(definition_groups[:body])&.to_json
                             else
-                              build_raw_payload(parameter_groups[:body])
+                              build_raw_payload(definition_groups[:body])
                             end
         request
       end
 
       private
 
-      def expand_parameters(metadata, openapi_spec, _example)
-        expandable_parameters = [
+      def expand_definitions(metadata, openapi_spec, _example)
+        expandable_definitions = [
           *metadata[:operation][:parameters],
           *metadata[:path_item][:parameters],
-          *derive_security_params(metadata, openapi_spec)
+          *derive_security_definitions(metadata, openapi_spec)
         ]
 
-        expandable_parameters
-          .map { |p| p['$ref'] ? resolve_parameter(p['$ref'], openapi_spec) : p }
-          .uniq { |p| p[:name] }
-          .reject { |p| p[:required] == false && !headers.key?(p[:name]) && !params.key?(p[:name]) }
+        expandable_definitions
+          .map { |d| d['$ref'] ? resolve_parameter(d['$ref'], openapi_spec) : d }
+          .uniq { |d| d[:name] }
+          .reject { |d| d[:required] == false && !headers.key?(d[:name]) && !param_values.key?(d[:name]) }
       end
 
-      def derive_security_params(metadata, openapi_spec)
+      def derive_security_definitions(metadata, openapi_spec)
         requirements = metadata[:operation][:security] || openapi_spec[:security] || []
         scheme_names = requirements.flat_map(&:keys)
         schemes = (openapi_spec.dig(:components, :securitySchemes) || {}).slice(*scheme_names).values
 
         schemes.map do |scheme|
-          param = scheme[:type] == :apiKey ? scheme.slice(:name, :in) : { name: 'Authorization', in: :header }
-          param.merge(schema: { type: :string }, required: requirements.one?)
+          definition = scheme[:type] == :apiKey ? scheme.slice(:name, :in) : { name: 'Authorization', in: :header }
+          definition.merge(schema: { type: :string }, required: requirements.one?)
         end
       end
 
@@ -98,22 +98,22 @@ module Rswag
         URI(base_path).path
       end
 
-      def build_path_string(metadata, openapi_spec, path_parameters)
+      def build_path_string(metadata, openapi_spec, path_definitions)
         template = base_path_from_servers(openapi_spec) + metadata[:path_item][:template]
-        path_parameters.each_with_object(template) do |p, path|
-          PathParameter.new(p, params[p[:name]]).sub_into_template!(path)
+        path_definitions.each_with_object(template) do |d, path|
+          PathParameter.new(d, param_values[d[:name]]).sub_into_template!(path)
         end
       end
 
-      def build_query_string(query_parameters)
-        query_strings = query_parameters.filter_map { |p| QueryParameter.new(p, params[p[:name]]).to_query }
+      def build_query_string(query_definitions)
+        query_strings = query_definitions.filter_map { |d| QueryParameter.new(d, param_values[d[:name]]).to_query }
         query_strings.any? ? "?#{query_strings.join('&')}" : ''
       end
 
-      def build_headers(metadata, openapi_spec, header_parameters, example)
+      def build_headers(metadata, openapi_spec, header_definitions, example)
         combined = openapi_spec.merge(metadata[:operation])
 
-        tuples = header_parameters.filter_map { |p| [p[:name], headers.fetch(p[:name]).to_s] }
+        tuples = header_definitions.filter_map { |d| [d[:name], headers.fetch(d[:name]).to_s] }
         tuples << ['Accept', headers.fetch('Accept', combined[:produces].first)] if combined[:produces]
         tuples << ['Content-Type', headers.fetch('Content-Type', combined[:consumes].first)] if combined[:consumes]
         tuples << ['Host', example.try(:Host) || combined[:host]] if combined[:host].present?
@@ -124,18 +124,18 @@ module Rswag
         end
       end
 
-      def build_form_payload(form_parameters)
+      def build_form_payload(form_definitions)
         # See http://seejohncode.com/2012/04/29/quick-tip-testing-multipart-uploads-with-rspec/
         # Rather that serializing with the appropriate encoding (e.g. multipart/form-data),
         # Rails test infrastructure allows us to send the values directly as a hash
         # PROS: simple to implement, CONS: serialization/deserialization is bypassed in test
-        form_parameters
-          .each_with_object({}) { |p, payload| payload[p[:name]] = params.fetch(p[:name]) }
+        form_definitions
+          .each_with_object({}) { |d, payload| payload[d[:name]] = param_values.fetch(d[:name]) }
       end
 
-      def build_raw_payload(body_parameters)
-        body_param = body_parameters.first || return
-        params.fetch(body_param[:name].to_s)
+      def build_raw_payload(body_definitions)
+        body_param = body_definitions.first || return
+        param_values.fetch(body_param[:name].to_s)
       rescue KeyError
         raise(MissingParameterError, body_param[:name])
       end
