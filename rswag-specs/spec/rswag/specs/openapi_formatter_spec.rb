7f4155cd5a4dd63d271d1224c072c4df20dd6cfc
Eike Send <eike.send@gmail.com>
2025-12-01 13:04:03 +0100

    Remove ostruct require statements
    The OpenStructs have previously been removed. The ostruct lib does not need to be required anymore. It's also not a part of the standardlib in ruby 4.0 anymore so it shouldn't be here by then.

--- rswag-specs/spec/rswag/specs/openapi_formatter_spec.rb
+++ rswag-specs/spec/rswag/specs/openapi_formatter_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'rswag/specs/openapi_formatter'
-require 'ostruct'
 
 module Rswag
   module Specs
--- rswag-ui/lib/rswag/ui/configuration.rb
+++ rswag-ui/lib/rswag/ui/configuration.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'ostruct'
 require 'rack'
 
 module Rswag
